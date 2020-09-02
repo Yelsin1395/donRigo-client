1c3d4ab56edd9358e713067d06672181aa1b1372
Jesús Yelsin Broly <yelgari13@gmail.com>
2020-09-02 15:03:06 -0500

    Prueba de la subida a firebase desde git hub.

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Main = () => {
-  return <span>Página principal</span>;
+  return (
+    <span>
+      Página principal que ya esta puesta con git hub y esta es una prueba que
+      mis conocimiento hasta aquí han avanzado bastante y estyo orgulloso de ti
+      Broly
+    </span>
+  );
 };
 
 export default Main;
